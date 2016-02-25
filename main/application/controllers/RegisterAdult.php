7547bf16ce9836d035b25dc5d1c92db0030ac2fa
aminjoon <maminbajand@yahoo.com>
2016-02-25 19:20:29 +0330

    Register Form Foundation Complete .
    ساختار جدیدی برای ساخت فرم ها ایجاد کردم تا به سبکی هر چه بیشتر صفحات
    لود شده بینجامد .
    با توجه به حجم بالای هر فرم ، باید فرم ها را تکه تکه کرد .
    اگر کنترلر مربوط را باز کنی متوجه میشوی

--- main/application/controllers/RegisterAdult.php
+++ main/application/controllers/RegisterAdult.php
@@ -11,21 +11,26 @@ class RegisterAdult extends CI_Controller
 {
     public function index()
     {
-        loadView('panel/RegisterAdult/RegisterAdult', array(),
+        loadView('panel/RegisterAdult/RegisterAdult',
             array(
-//                'plugins/jdatepicker/skins/calendar-blue.css',
-
-                'plugins/jdatepicker/skins/aqua/theme.css'
+                'part1'=>$this->load->view('panel/RegisterAdult/Part1',array(),true),
+                'part2'=>$this->load->view('panel/RegisterAdult/Part2',array(),true),
+                'part3'=>$this->load->view('panel/RegisterAdult/Part3',array(),true)
+            ),
+            array(
+                'plugins/jdatepicker/skins/aqua/theme.css',
+                'plugins/iCheck/all.css'
             ),
             array(
                 'plugins/jdatepicker/jalali.js',
                 'plugins/jdatepicker/calendar.js',
                 'plugins/jdatepicker/calendar-setup.js',
                 'plugins/jdatepicker/lang/calendar-fa.js',
+                'plugins/iCheck/icheck.min.js',
+                'jsclass/RegisterAdult/FormHelper.js'
             )
         );
     }
-
 }
 
 ?>
--- main/application/views/panel/RegisterAdult/RegisterAdult.php
+++ main/application/views/panel/RegisterAdult/RegisterAdult.php
@@ -1,50 +1,27 @@
+<script>
+    $(function () {
+        formHelper(3);
+        //iCheck for checkbox and radio inputs
+        $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
+            checkboxClass: 'icheckbox_minimal-blue',
+            radioClass: 'iradio_minimal-blue'
+        });
+    });
+</script>
 <section class="content">
     <div class="row">
         <div class="box box-info">
-            <div class="box-header with-border">
-                <h3 class="box-title">اطلاعات شخصی</h3>
-            </div><!-- /.box-header -->
+
             <!-- form start -->
             <form class="form-horizontal">
-                <div class="box-body">
-                    <div class="form-group">
-                        <label for="firstName" class="col-sm-2 control-label">نام</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" name="firstName" placeholder="">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="lastName" class="col-sm-2 control-label">نام خانوادگی</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" name="lastName" placeholder="">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="gender" class="col-sm-2 control-label">جنسیت</label>
-                        <div class="col-sm-10">
-                            <input type="radio" name="gender" value="male"> مذکر
-                            <input type="radio" name="gender" value="female"> مونث
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="birthday" class="col-sm-2 control-label">تاريخ تولد</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" name="birthday" id="birthday2" placeholder="" value=""><img id="date_btn_1" src="<?=base_url('assets/panel_assets/images/calendar24.png');?>" />
-                        </div>
-                        <script type="text/javascript">
-                            Calendar.setup({
-                                inputField     :    "birthday2",
-                                button         :    "date_btn_1",
-                                ifFormat       :    "%Y-%m-%d",
-                                dateType	   :	'jalali',
-                                weekNumbers    : false
-                            });
-                        </script>
-                    </div>
-                    ...
-                </div><!-- /.box-body -->
+                <?=$part1?>
+                <?=$part2?>
+                <?=$part3?>
                 <div class="box-footer">
-                    <button type="submit" class="btn btn-info pull-right">مرحله بعد</button>
+                    <button type="button" class="btn btn-info pull-right" onclick="nextStepRegister();">مرحله بعد
+                    </button>
+                    <button type="submit" id="submit" class="btn btn-danger pull-right" style="display: none;">تکمیل ثبت نام
+                    </button>
                 </div><!-- /.box-footer -->
             </form>
         </div>
