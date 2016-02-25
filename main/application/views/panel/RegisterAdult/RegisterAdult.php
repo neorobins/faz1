d8aa79b7a9bae49b57911269f82f260058c9ef7e
Neo Robins <neorobins@gmail.com>
2016-02-25 18:03:39 +0330

    register v1

--- main/application/views/panel/RegisterAdult/RegisterAdult.php
+++ main/application/views/panel/RegisterAdult/RegisterAdult.php
@@ -9,35 +9,48 @@
                 <div class="box-body">
                     <div class="form-group">
                         <label for="firstName" class="col-sm-2 control-label">نام</label>
+
                         <div class="col-sm-10">
                             <input type="text" class="form-control" name="firstName" placeholder="">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="lastName" class="col-sm-2 control-label">نام خانوادگی</label>
+
                         <div class="col-sm-10">
                             <input type="text" class="form-control" name="lastName" placeholder="">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="gender" class="col-sm-2 control-label">جنسیت</label>
+
                         <div class="col-sm-10">
                             <input type="radio" name="gender" value="male"> مذکر
                             <input type="radio" name="gender" value="female"> مونث
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="birthday" class="col-sm-2 control-label">تاريخ تولد</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" name="birthday" id="birthday2" placeholder="" value=""><img id="date_btn_1" src="<?=base_url('assets/panel_assets/images/calendar24.png');?>" />
+
+                        <div class="box-body row">
+                            <label for="birthday" class="col-xs-2 control-label">تاريخ تولد</label>
+
+                            <div class="col-xs-9">
+                                <input type="text" class="form-control" name="birthday" id="birthday2" placeholder=""
+                                       value="">
+                            </div>
+                            
+                            <div class="col-xs-1">
+                                <img id="date_btn_1"
+                                     src="<?= base_url('assets/panel_assets/images/calendar24.png'); ?>"/>
+                            </div>
                         </div>
                         <script type="text/javascript">
                             Calendar.setup({
-                                inputField     :    "birthday2",
-                                button         :    "date_btn_1",
-                                ifFormat       :    "%Y-%m-%d",
-                                dateType	   :	'jalali',
-                                weekNumbers    : false
+                                inputField: "birthday2",
+                                button: "date_btn_1",
+                                ifFormat: "%Y-%m-%d",
+                                dateType: 'jalali',
+                                weekNumbers: false
                             });
                         </script>
                     </div>
